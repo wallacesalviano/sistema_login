29ac29d64868fd8e97342c12a46fba1adfa37148
wallace salviano <wallacefs067@gmail.com>
2023-09-17 16:28:39 -0300

    adicionado a imagem e campos de obrigatoriedade

--- main.py
+++ main.py
@@ -1,33 +1,45 @@
 import customtkinter as ctk
+import tkinter as tk
+from PIL import Image, ImageTk 
 
 # Estilização da nossa janela
 ctk.set_appearance_mode('dark')
 ctk.set_default_color_theme('dark-blue')
 
 janela = ctk.CTk()
-janela.geometry("700x400")
+janela.geometry("800x500")
 janela.resizable(False, False)
+janela.title("Sistema de Login")
 
 def clique():
-    print('Fazer Login')
+    print('Fazer Login')    
 
+# Tratamento da Imagem Central
+image = Image.open("imagem4.png") 
+image = ImageTk.PhotoImage(image)
 
-title = ctk.CTkLabel(janela, text="Sistema de login")
-title.pack(padx=10, pady=10)
+image_label = tk.Label(janela, image=image)
+image_label.place(x=-100, y=0)
 
-email = ctk.CTkEntry(janela, placeholder_text="E-mail")
-email.pack(padx=10, pady=10)
+# Frame
+frame = ctk.CTkFrame(janela, width=380, height=500, border_width=2)
+frame.pack(side="right")
 
-senha = ctk.CTkEntry(janela, placeholder_text="Senha", show="*")
-senha.pack(padx=10, pady=10)
+# Frame Widgets
+label = ctk.CTkLabel(frame, text="Sistema de Login")
+label.place(x=65, y=35)
+label.configure(font=("Lato", 30))
 
-check = ctk.CTkCheckBox(janela, text="Lembre sua senha")
-check.pack()
+# Área do login do cliente
+entry1 = ctk.CTkEntry(frame, placeholder_text="Usuário", width=300).place(x=40, y=115)
+label1 = ctk.CTkLabel(frame, text_color="#3025D4", text="*O campo usuário é obrigatório*").place(x=40, y=145)
 
-# Janela do botão com comando pra função clique
-botao = ctk.CTkButton(janela, text="Login", command="clique")
-botao.pack(padx=10, pady=10)
+entry2 = ctk.CTkEntry(frame, placeholder_text="Senha", width=300).place(x=40, y=185)
+label2 = ctk.CTkLabel(frame, text_color="#3025D4", text="*O campo usuário é obrigatório*").place(x=40, y=215)
 
+check_box = ctk.CTkCheckBox(frame, text="Lembrar a senha").place(x=40, y=255)
+
+button = ctk.CTkButton(frame, text="LOGIN", width=300).place(x=40, y=300)
 
 
 janela.mainloop()
